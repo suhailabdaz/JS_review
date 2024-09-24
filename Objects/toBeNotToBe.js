1331be60e856088873e11375ffce5f1bcac81869
suhailabdaz <suhail.abdaz@gmail.com>
2024-09-24 13:20:28 +0530

    feat: new boarding 1 branch

---
+++ Objects/toBeNotToBe.js
@@ -0,0 +1,13 @@
+function expect(val){
+  let obj={
+    toBe(newVal){
+     return val === newVal
+    },
+    notToBe(newVal){
+     return val !== newVal
+    }
+  }
+  return obj
+}
+
+console.log(expect(5).notToBe(8))
